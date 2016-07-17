e6fe617e426c21139accd99df8b56e5621217eb6
ssong <songstephanie03@gmail.com>
2016-07-17 13:35:09 -0700

    Completed dictionaries assessment

---
+++ assessment.py
@@ -0,0 +1,272 @@
+"""Dictionaries Assessment
+
+**IMPORTANT:** these problems are meant to be solved using
+dictionaries and sets.
+"""
+
+def count_words(phrase):
+    """Count unique words in a string.
+
+    This function should take a single string and return a dictionary
+    that has all of the distinct words as keys, and the number of
+    times that word appears in the string as values.
+
+    For example::
+
+        >>> print_dict(count_words("each word appears once"))
+        {'appears': 1, 'each': 1, 'once': 1, 'word': 1}
+
+    Words that appear more than once should be counted each time::
+
+        >>> print_dict(count_words("rose is a rose is a rose"))
+        {'a': 2, 'is': 2, 'rose': 3}
+
+    It's fine to consider punctuation part of a word (e.g., a comma
+    at the end of a word can be counted as part of that word) and
+    to consider differently-capitalized words as different::
+
+        >>> print_dict(count_words("Porcupine see, porcupine do."))
+        {'Porcupine': 1, 'do.': 1, 'porcupine': 1, 'see,': 1}
+    """
+    dict_of_words = {}
+
+    list_of_words = phrase.split(" ")
+
+    for word in list_of_words:
+        if word not in dict_of_words:
+            dict_of_words[word] = 1
+        else:
+            dict_of_words[word] += 1
+
+    return dict_of_words
+
+
+def get_melon_price(melon_name):
+    """Given a melon name, return the price of the melon
+
+    Here are a list of melon names and prices:
+    Watermelon 2.95
+    Cantaloupe 2.50
+    Musk 3.25
+    Christmas 14.25
+    (it was a bad year for Christmas melons -- supply is low!)
+
+        >>> get_melon_price('Watermelon')
+        2.95
+
+        >>> get_melon_price('Musk')
+        3.25
+
+        >>> get_melon_price('Tomato')
+        'No price found'
+    """
+    melons = {
+        "Watermelon" : 2.95,
+        "Cantaloupe": 2.50,
+        "Musk": 3.25,
+        "Christmas": 14.25
+    }
+
+    #search the dictionary to see if melon_name is in there. If so, return
+    #the price. Otherwise, say no price found
+
+    if melon_name in melons:
+        return melons[melon_name]
+    else:
+        return "No price found"
+
+
+def word_length_sorted(words):
+    """Return list of word-lengths and words.
+
+    Given a list of words, return a list of tuples, ordered by
+    word-length. Each tuple should have two items --- a number that
+    is a word-length, and the list of words of that word length.
+
+    In addition to ordering the list by word length, order each
+    sub-list of words alphabetically.
+
+    For example::
+
+        >>> word_length_sorted(["ok", "an", "apple", "a", "day"])
+        [(1, ['a']), (2, ['an', 'ok']), (3, ['day']), (5, ['apple'])]
+    """
+    dict_of_lengths = {}
+
+    for word in words:
+        #check to see if length of the word is in the dict
+        if len(word) not in dict_of_lengths:
+            #adding empty list as initial setup and appending the word to the []
+            dict_of_lengths[len(word)] = []
+            dict_of_lengths[len(word)].append(word)
+            #appends word to the list that already contains values
+        else:
+            dict_of_lengths[len(word)].append(word)
+
+    #alphabetizing each list in the dict
+    for l in dict_of_lengths:
+        dict_of_lengths[l].sort()
+
+    #calling items() on dict when we return so we get the format we want
+    return dict_of_lengths.items()
+
+
+def translate_to_pirate_talk(phrase):
+    """Translate phrase to pirate talk.
+
+    Given a phrase, translate each word to the Pirate-speak
+    equivalent. Words that cannot be translated into Pirate-speak
+    should pass through unchanged. Return the resulting sentence.
+
+    Here's a table of English to Pirate translations:
+
+    ----------  ----------------
+    English     Pirate
+    ----------  ----------------
+    sir         matey
+    hotel       fleabag inn
+    student     swabbie
+    man         matey
+    professor   foul blaggart
+    restaurant  galley
+    your        yer
+    excuse      arr
+    students    swabbies
+    are         be
+    restroom    head
+    my          me
+    is          be
+    ----------  ----------------
+
+    For example::
+
+        >>> translate_to_pirate_talk("my student is not a man")
+        'me swabbie be not a matey'
+
+    You should treat words with punctuation as if they were different
+    words::
+
+        >>> translate_to_pirate_talk("my student is not a man!")
+        'me swabbie be not a man!'
+    """
+
+    pirated = ""
+
+    pirate_dict = {
+        "sir": "matey",
+        "hotel": "fleabag inn",
+        "student": "swabbie",
+        "man": "matey",
+        "professor": "foul blaggart",
+        "restaurant": "galley",
+        "your": "yer",
+        "excuse": "arr",
+        "students": "swabbies",
+        "are": "be",
+        "restroom": "head",
+        "my": "me",
+        "is": "be"
+    }
+
+    #create list of the words in the phrase
+    words = phrase.split(" ")
+
+    for w in words:
+        #if the word is in the dict, add the pirate-speak word, otherwise
+        #just add the word
+        if w in pirate_dict:
+            pirate_word = pirate_dict[w]
+            pirated += pirate_word + " "
+        else:
+            pirated += w + " "
+
+    #rstrip the phrase to get rid of the blank space at the end
+    return pirated.rstrip()
+
+
+def kids_game(names):
+    """Play a kids' word chain game.
+
+    Given a list of names, like::
+
+      bagon baltoy yamask starly nosepass kalob nicky
+
+    Do the following:
+
+    1. Always start with the first word ("bagon", in this example).
+
+    2. Add it to the results.
+
+    3. Use the last letter of that word to look for the next word.
+       Since "bagon" ends with n, find the *first* word starting
+       with "n" in our list --- in this case, "nosepass".
+
+    4. Add "nosepass" to the results, and continue. Once a word has
+       been used, it can't be used again --- so we'll never get to
+       use "bagon" or "nosepass" a second time.
+
+    5. When you can't find an unused word to use, you're done!
+       Return the list of output words.
+
+    For example::
+
+        >>> kids_game(["bagon", "baltoy", "yamask", "starly",
+        ...            "nosepass", "kalob", "nicky", "booger"])
+        ['bagon', 'nosepass', 'starly', 'yamask', 'kalob', 'baltoy']
+
+    (After "baltoy", there are no more y-words, so we end, even
+    though "nicky" and "booger" weren't used.)
+
+    This is a tricky problem. In particular, think about how using
+    a dictionary (with the super-fast lookup they provide) can help;
+    good solutions here will definitely require a dictionary.
+    """
+    chain = []
+    letter_choices = {}
+
+    #since we always start with the first word, pop the first word and
+    #add it to the chain
+    chain.append(names.pop(0))
+
+    #create a dictionary with letters as the key and words that start
+    #with the letter as the values
+    for word in names:
+        if word[0] not in letter_choices:
+            letter_choices[word[0]] = []
+            letter_choices[word[0]].append(word)
+        else:
+            letter_choices[word[0]].append(word)
+
+    #keep looping as long as the last letter of the last word in chain is in the
+    #dict and the list associated with that letter has words in it
+    while chain[-1][-1] in letter_choices and len(letter_choices[chain[-1][-1]]) > 0:
+        #add the word to the chain and pop it so we don't reuse words
+        chain.append(letter_choices[chain[-1][-1]].pop(0))
+
+    return chain
+
+#####################################################################
+# You can ignore everything below this.
+
+def print_dict(d):
+    # This method is used to print dictionaries in key-alphabetical
+    # order, and is only for our doctests. You can ignore it.
+    if isinstance(d, dict):
+        print "{" + ", ".join(
+            "%r: %r" % (k, d[k]) for k in sorted(d)) + "}"
+    else:
+        print d
+
+
+def sort_pairs(l):
+    # Print sorted list of pairs where the pairs are sorted. This is
+    # used only for documentation tests. You can ignore it.
+    return sorted(sorted(pair) for pair in l)
+
+
+if __name__ == "__main__":
+    print
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "*** ALL TESTS PASSED ***"
+    print
